267d9b08ab8d24b72130075f8aa2f2360e695b41
smuke <smuke@smuke.dev>
2025-02-01 14:21:54 -0800

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,67 @@
+import { useEffect, useState } from "react";
+import "./styles.css";
+
+export default function App() {
+  const [time, setTime] = useState(0);
+  const [isRunning, setIsRunning] = useState(false);
+  const [isDarkMode, setIsDarkMode] = useState(false);
+
+  useEffect(() => {
+    let it;
+
+    if (isRunning) {
+      it = setInterval(() => {
+        setTime((prevTime) => {
+          if (prevTime === 12) {
+            return 0;
+          }
+
+          return prevTime + 1;
+        });
+      }, 1000);
+    } else {
+      clearInterval(it);
+    }
+
+    return () => clearInterval(it);
+  }, [isRunning]);
+
+  useEffect(() => {
+    if (time >= 6) setIsDarkMode(true);
+    else setIsDarkMode(false);
+  }, [time]);
+
+  const handleStart = () => {
+    setIsRunning(true);
+  };
+
+  const handleStop = () => {
+    setIsRunning(false);
+  };
+
+  const handleReset = () => {
+    setIsRunning(false);
+    setTime(0);
+  };
+
+  return (
+    <center>
+      <p>{time}</p>
+      <button onClick={handleStart}>Start</button>
+      <button onClick={handleStop}>Stop</button>
+      <button onClick={handleReset}>Reset</button>
+
+      <div
+        style={{
+          color: "red",
+          border: "1px solid gray",
+          margin: "20px",
+          padding: "20px",
+          background: isDarkMode ? "black" : "white",
+        }}
+      >
+        Dark Mode: {isDarkMode ? "ON" : "OFF"}
+      </div>
+    </center>
+  );
+}
